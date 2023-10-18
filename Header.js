8696896ed813d36c813155fa3d683a05ab3e4877
ThanooshDarsi <94456829+ThanooshDarsi@users.noreply.github.com>
2023-10-18 21:57:47 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Darsi Venkata Sai Thanoosh</h1>
+      <div className="tile">
+        <h3>Front End Developer</h3>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Skills.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Skills.css';
+
+function Skills() {
+  return (
+    <section className="skills">
+        <br></br>
+      <h2>Skills</h2>
+      <ul>
+        <li>Web Development</li>
+        <li>Mobile Application Development</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>Data Analysis using R</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
